d862cfa7c22fd5a1c9a809e85b8142938e974f23
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-29 16:18:52 -0400

    Create TimesheetCollection #location_names

--- lib/timesheet_collection.rb
+++ lib/timesheet_collection.rb
@@ -15,10 +15,16 @@ class TimesheetCollection
     new(timesheet_collection)
   end
 
-  def calc_tot_employee_hours
+  def calc_tot_hours_by_employee
     collection.each_with_object({}) do |tsheet, hash|
       hash.default = 0
       hash[tsheet.name] += tsheet.hours
+    end
+  end
+
+  def location_names
+    @collection.each_with_object([]) do |tsheet, arr|
+      arr << tsheet.location unless arr.include?(tsheet.location)
     end
   end
 end
--- runner.rb
+++ runner.rb
@@ -4,7 +4,8 @@ require './lib/timesheet_collection'
 data = CSV.read('./data/payroll_9_25_20.csv', headers: true, header_converters: :symbol)
 payroll_data = TimesheetCollection.from_csv(data)
 
-pp payroll_data.calc_tot_employee_hours
+# pp payroll_data.calc_tot_employee_hours
+pp payroll_data.location_names
 
 #  Open file
 #  Convert to csv object
