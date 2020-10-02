b7d4ca85b8e96edd076d918a46c1f6904d60787f
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-02 17:14:36 -0400

    Organize and refactor methods

--- lib/timesheet_collection.rb
+++ lib/timesheet_collection.rb
@@ -23,62 +23,46 @@ class TimesheetCollection
     collection.map(&:name).uniq
   end
 
-  def tsheets_by_employee(employee_name)
+  def filter_tsheets_by_employee(employee_name)
     collection.find_all { |tsheet| tsheet.name == employee_name }
   end
 
-  def tot_hours_by_employee(employee_name)
-    tsheets_by_employee(employee_name).map(&:hours).sum
+  def total_employee_hours(employee_name)
+    filter_tsheets_by_employee(employee_name).map(&:hours).sum
   end
 
-  def employee_hours_by_job2
-    employee_names.each_with_object({}) do |employee_name, employee_summary|
-      employee_summary[employee_name] = hours_by_job2(employee_name)
-    end
-  end
-
-  def hours_by_job2(employee_name)
-    collection.each_with_object({}) do |tsheet, job_summary|
-      job_summary.default = 0
-      job_summary[tsheet.job] += tsheet.hours if tsheet.name == employee_name
-    end
-  end
-
-  def hours_by_job(employee_name)
-    tsheets_by_employee(employee_name).each_with_object({}) do |tsheet, hash|
+  def employee_hours_by_job(employee_name)
+    filter_tsheets_by_employee(employee_name).each_with_object({}) do |tsheet, hash|
       hash.default = 0
       hash[tsheet.job] += tsheet.hours
     end
   end
 
-  def percentage_by_job(employee_name)
-    job_hours = hours_by_job(employee_name)
-    total_hours = tot_hours_by_employee(employee_name)
-
-    job_names.each_with_object({}) do |job_name, hash|
-      hash[job_name] = (job_hours[job_name] / total_hours).round(2)
-    end
-  end
-
-  def employee_hours_by_job
+  def employee_hours_summary
     employee_names.each_with_object({}) do |name, hash|
-      hash[name] = hours_by_job(name)
+      hash[name] = employee_hours_by_job(name)
     end
   end
 
-  def employee_percentage_by_job
+  def employee_percentage_by_job(employee_name)
+    employee_hours_by_job(employee_name).transform_values do |job_hours|
+      (job_hours / total_employee_hours(employee_name)).round(2)
+    end
+  end
+
+  def employee_percentage_summary
     employee_names.each_with_object({}) do |name, hash|
-      hash[name] = percentage_by_job(name)
+      hash[name] = employee_percentage_by_job(name)
     end
   end
 
-  def tot_hours
+  def total_hours
     employee_names.reduce(0) do |sum, name|
-      sum + tot_hours_by_employee(name)
+      sum + total_employee_hours(name)
     end
   end
 
-  def tot_hours_by_job
+  def 
     job_names.each_with_object({}) do |job_name, hash|
       hash[job_name] = collection.select { |tsheet| tsheet.job == job_name }.map(&:hours).sum
     end
@@ -86,7 +70,7 @@ class TimesheetCollection
 
   def edison_percentage_by_job
     job_names.each_with_object({}) do |name, hash|
-      hash[name] = (tot_hours_by_job[name] / tot_hours).round(2)
+      hash[name] = (total_hours_by_job[name] / total_hours).round(2)
     end
   end
 
--- runner.rb
+++ runner.rb
@@ -5,17 +5,18 @@ data = CSV.read(ARGV[0], headers: true, header_converters: :symbol)
 payroll_data = TimesheetCollection.from_csv(data)
 
 # pp payroll_data.job_names
-pp payroll_data.employee_names
+# pp payroll_data.employee_names
 # pp payroll_data.tot_hours_by_employee('Carlos')
 # pp payroll_data.tot_hours_by_employee('Stevan')
 # pp payroll_data.tot_hours_by_employee('Erikson')
 # pp payroll_data.tsheets_by_employee('Carlos')
 # pp payroll_data.hours_by_job('Carlos')
 # pp payroll_data.percentage_by_job('Carlos')
-# pp payroll_data.employee_hours_by_job
+# pp payroll_data.employee_hours_summary
+# pp payroll_data.employee_percentage_summary
 # pp payroll_data.employee_percentage_by_job_all
 # pp payroll_data.tot_hours
-# pp payroll_data.tot_hours_by_job
+pp payroll_data.total_hours_by_job
 # pp payroll_data.edison_percentage_by_job
 
 # pp payroll_data.employee_hours_by_job2
