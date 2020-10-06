4cff6f7e92f125525520e1ba1ccdbdd5968d5f71
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-06 17:20:01 -0400

    Write tests for Timesheet

---
+++ spec/timesheet_spec.rb
@@ -0,0 +1,23 @@
+require './lib/timesheet'
+
+describe 'Timesheet' do
+  subject { Timesheet.new({ firstname: 'Carlos', total_hours: '6', location_code: '86' }) }
+
+  describe '#init' do
+    it 'is an instance of Timesheet' do
+      is_expected.to be_an_instance_of Timesheet
+    end
+
+    it 'has a name' do
+      expect(subject.name).to eql('Carlos')
+    end
+
+    it 'has hours' do
+      expect(subject.hours).to eql(6.0)
+    end
+
+    it 'has a job' do
+      expect(subject.job).to eql('86')
+    end
+  end
+end
