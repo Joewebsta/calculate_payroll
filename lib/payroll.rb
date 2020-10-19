1c641286b8c965697ebd8d87f2063aa7674a5d34
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-19 11:44:46 -0400

    Create Payroll class

---
+++ lib/payroll.rb
@@ -0,0 +1,17 @@
+class Payroll
+  attr_reader :name,
+              :gross,
+              :net,
+              :ee_taxes,
+              :er_taxes,
+              :gross_er_sum
+
+  def initialize(data)
+    @name = data[:first_name]
+    @gross = data[:gross_earnings].to_f
+    @net = data[:net_pay].to_f
+    @ee_taxes = data[:employee_taxes].to_f
+    @er_taxes = data[:employer_taxes].to_f
+    @gross_er_sum = @gross + @er_taxes
+  end
+end
