242ade83845f8e6c7f43c75c673a06af82876b3f
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-20 18:27:33 -0400

    Add garnishment instance variable

--- lib/payroll.rb
+++ lib/payroll.rb
@@ -2,6 +2,7 @@ class Payroll
   attr_reader :name,
               :gross,
               :net,
+              :garnishment,
               :ee_taxes,
               :er_taxes,
               :gross_er_sum
@@ -10,6 +11,7 @@ class Payroll
     @name = data[:first_name] || 'Totals'
     @gross = data[:gross_earnings].to_f
     @net = data[:net_pay].to_f
+    @garnishment = data[:garnishment].to_f
     @ee_taxes = data[:employee_taxes].to_f
     @er_taxes = data[:employer_taxes].to_f
     @gross_er_sum = @gross + @er_taxes
