341b05f435ff33296907aa51d21f58a36ca568e9
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-20 18:53:40 -0400

    Print #employee_payroll_summary to the command line

--- runner.rb
+++ runner.rb
@@ -1,19 +1,25 @@
 require 'csv'
 require './lib/timesheet_collection'
+require './lib/payroll_collection'
 require './lib/report_formatter'
 
 timesheet_data = CSV.read(ARGV[0], headers: true, header_converters: :symbol)
 payroll_data = CSV.read(ARGV[1], headers: true, header_converters: :symbol)
 
-timesheet_collection_data = TimesheetCollection.from_csv(timesheet_data)
-timesheet_report = timesheet_collection_data.employee_percentage_summary_with_edison
-# formatter = ReportFormatter.new(timesheet_report)
+payroll_collection = PayrollCollection.from_csv(payroll_data)
+timesheet_collection = TimesheetCollection.from_csv(timesheet_data)
 
+# timesheet_report = timesheet_collection.employee_percentage_summary_with_edison
+# formatter = ReportFormatter.new(timesheet_report)
 # File.open('text/timesheet_report.txt', 'w').write(formatter.format_report)
 
-# pp timesheet_collection_data.employee_hours_summary
-# pp timesheet_collection_data.employee_percentage_by_job('Carlos')
-# pp timesheet_collection_data.employee_payroll_by_job('Carlos')
+# TEST CODE
+# pp timesheet_collection.employee_hours_summary
+# pp timesheet_collection.employee_percentage_by_job('Carlos')
+# pp timesheet_collection.employee_payroll_by_job('Carlos')
+# pp payroll_data
+# pp timesheet_collection.employee_payroll_by_job('Carlos', payroll_collection)
+pp timesheet_collection.employee_payroll_summary(payroll_collection)
 
 # TODO
 # give jobs an id so they are orded consitently in output text file
